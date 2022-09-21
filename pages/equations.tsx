fe23bf31fea26432980da943e44e0ec666e7a539
Lucas Kim <lucaskamadakim@gmail.com>
2022-09-20 22:06:45 -0400

    Add interactive graph

--- pages/equations.tsx
+++ pages/equations.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 
-/* const Algebrite = require("algebrite"); */
-
 const Equations: NextPage = () => {
   return <h1>Hi</h1>;
 };
--- pages/graph.tsx
+++ pages/graph.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import paths from "../data/paths.json";
+import * as d3 from "d3";
 
 type SavedQuestion = {
   question: string;
@@ -22,17 +22,41 @@ type Option = {
 
 type Equation = {
   equation: string;
+  parameters: Parameter[];
 };
+
+type Parameter = {
+  symbol: string;
+  min?: number;
+  max?: number;
+  value: number;
+  requirements: Requirement[];
+};
+
+type D3SVG = d3.Selection<
+  SVGGElement,
+  [number, number][],
+  HTMLElement,
+  undefined
+>;
+
+type Requirement = "even" | "odd";
+
+interface ParameterValues {
+  [name: string]: number;
+}
 
 const parsedPaths = paths as Question;
 
-const Equations: NextPage = () => {
+const Graph: NextPage = () => {
   const [completedQuestions, setCompletedQuestions] = useState<SavedQuestion[]>(
     []
   );
   const [currentPath, setCurrentPath] = useState<Question | Equation>(
     parsedPaths
   );
+
+  const [parameterValues, setParameterValues] = useState<ParameterValues>({});
 
   const handleOptionClick = (j: number) => {
     const currentQuestion = currentPath as Question;
@@ -42,8 +66,98 @@ const Equations: NextPage = () => {
       chosenOption: j,
     };
     setCompletedQuestions([...completedQuestions, newlySavedQuestion]);
-    setCurrentPath(currentQuestion.options[j].child);
+
+    const newPath = currentQuestion.options[j].child;
+    setCurrentPath(newPath);
+
+    if ("equation" in newPath) {
+      newPath.parameters.forEach((parameter) => {
+        if (!(parameter.symbol in parameterValues)) {
+          setParameterValues({
+            ...parameterValues,
+            [parameter.symbol]: parameter.value,
+          });
+        }
+      });
+    }
   };
+
+  const domain = [-10, 10];
+  const margin = { top: 20, right: 50, bottom: 50, left: 50 };
+  const width = 450 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+  const x = d3.scaleLinear().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
+  const graph = useRef<D3SVG>();
+  const xAxisGroup = useRef<D3SVG>();
+  const yAxisGroup = useRef<D3SVG>();
+  const path =
+    useRef<
+      d3.Selection<SVGPathElement, [number, number][], HTMLElement, undefined>
+    >();
+  const line = d3
+    .line()
+    .x((d) => x(d[0]))
+    .y((d) => y(d[1]));
+
+  useEffect(() => {
+    // Create plot
+    const svg = d3
+      .select("#root")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+
+    graph.current = svg
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + margin.left + "," + margin.top + ")"
+      ) as D3SVG;
+
+    path.current = graph.current.append("path");
+  }, [height, margin.bottom, margin.left, margin.right, margin.top, width]);
+
+  useEffect(() => {
+    if ("equation" in currentPath) {
+      let equation = currentPath.equation;
+      Object.entries(parameterValues).forEach(([key, value]) => {
+        equation = equation.replace(key, value.toString());
+      });
+
+      const f = (x: number) => {
+        return eval(equation);
+      };
+
+      const data: [number, number][] = [];
+      for (let xValue = domain[0]; xValue <= domain[1]; xValue += 0.1) {
+        const yValue = f(xValue);
+        data.push([xValue, yValue]);
+      }
+
+      x.domain([-10, 10]);
+      y.domain([-10, 10]);
+
+      xAxisGroup.current = graph
+        .current!.append("g")
+        .attr("transform", `translate(0,${y(0)})`);
+      yAxisGroup.current = graph
+        .current!.append("g")
+        .attr("transform", `translate(${x(0)},0)`);
+
+      const xAxis = d3.axisBottom(x);
+      const yAxis = d3.axisLeft(y);
+
+      xAxis(xAxisGroup.current!);
+      yAxis(yAxisGroup.current!);
+
+      path
+        .current!.attr("d", line(data))
+        .attr("fill", "none")
+        .attr("stroke", "teal")
+        .attr("stroke-width", 2);
+      console.log("hi");
+    }
+  });
 
   return (
     <>
@@ -83,19 +197,33 @@ const Equations: NextPage = () => {
         </div>
       ) : (
         <>
-          <Image
-            alt="Graph"
-            src={`/${completedQuestions
-              .map((question) => question.chosenOption)
-              .join("-")}.png`}
-            width={710}
-            height={441}
-          />
           <p>{currentPath.equation}</p>
+          {currentPath.parameters.map((parameter) => {
+            return (
+              <Form.Control
+                defaultValue={parameter.value}
+                key={parameter.symbol}
+                onChange={({ target }) =>
+                  setParameterValues({
+                    ...parameterValues,
+                    [parameter.symbol]: parseInt(target.value),
+                  })
+                }
+                min={parameter.min}
+                max={parameter.max}
+                type="number"
+                value={parameterValues[parameter.symbol]}
+              />
+            );
+          })}
         </>
       )}
+      <svg
+        id="root"
+        onClick={() => setCurrentPath({ ...currentPath, equation: "x**3" })}
+      ></svg>
     </>
   );
 };
 
-export default Equations;
+export default Graph;
